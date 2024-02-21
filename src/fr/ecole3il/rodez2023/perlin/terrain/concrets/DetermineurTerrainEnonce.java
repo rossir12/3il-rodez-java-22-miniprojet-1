bdbf23f8a60bb94e9e1abddc3e19ee93526aacfb
RRossi81 <151721460+rossir12@users.noreply.github.com>
2024-02-21 19:12:07 +0100

    Commentaires pour le package Concrets

--- src/fr/ecole3il/rodez2023/perlin/terrain/concrets/DetermineurTerrainEnonce.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/concrets/DetermineurTerrainEnonce.java
@@ -4,45 +4,51 @@ import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
 import fr.ecole3il.rodez2023.perlin.terrain.visualisation.DetermineurTerrain;
 
 public class DetermineurTerrainEnonce implements DetermineurTerrain {
-
+	/**
+	 * Détermine le type de terrain basé sur les paramètres
+	 * @param altitude Altitude du terrain
+	 * @param hydrometrie Hydrométrie du terrain
+	 * @param temperature température du terrain
+	 * @return Le TypeTerrain correspondant aux critères spécifiés
+	 */
 	@Override
 	public TypeTerrain determinerTerrain(double altitude, double hydrometrie, double temperature) {
 		if (altitude < 0) {
-			return TypeTerrain.OCEAN;
+			return TypeTerrain.OCEAN; // Altitude inférieur à 0 = ocean
 		} else if (hydrometrie <= 0.25) {
 			if (altitude <= 0.7) {
 				if (temperature <= 0.25) {
-					return TypeTerrain.PLAINE;
+					return TypeTerrain.PLAINE; // Terrain froids et secs
 				} else if (temperature <= 0.7) {
-					return TypeTerrain.FORET_FEUILLUS;
+					return TypeTerrain.FORET_FEUILLUS; // Terrain tempérés et secs
 				} else {
-					return TypeTerrain.TOUNDRA;
+					return TypeTerrain.TOUNDRA; // Terrains chauds et secs
 				}
 			} else {
 				if (temperature <= 0.25) {
-					return TypeTerrain.PLAINE;
+					return TypeTerrain.PLAINE; // Terrains froids et secs en haute altitude
 				} else if (temperature <= 0.7) {
-					return TypeTerrain.FORET_CONIFERES;
+					return TypeTerrain.FORET_CONIFERES; // Terrains tempérées et secs en haute altitude
 				} else {
-					return TypeTerrain.MONTAGNE;
+					return TypeTerrain.MONTAGNE; // Terrains chauds et secs en haute altitude
 				}
 			}
 		} else {
 			if (altitude <= 0.7) {
 				if (temperature <= 0.25) {
-					return TypeTerrain.DESERT;
+					return TypeTerrain.DESERT; // Terrains froids et humides
 				} else if (temperature <= 0.7) {
-					return TypeTerrain.COLLINES;
+					return TypeTerrain.COLLINES; // Terrains tempérés et humides
 				} else {
-					return TypeTerrain.MONTAGNE;
+					return TypeTerrain.MONTAGNE; // Terrains chauds et humides
 				}
 			} else {
 				if (temperature <= 0.25) {
-					return TypeTerrain.MARAIS;
+					return TypeTerrain.MARAIS; // Terrains froids et humides (haute altitude)
 				} else if (temperature <= 0.7) {
-					return TypeTerrain.FORET_CONIFERES;
+					return TypeTerrain.FORET_CONIFERES; // Terrains tempérés et humides (haute altitude)
 				} else {
-					return TypeTerrain.MONTAGNE;
+					return TypeTerrain.MONTAGNE; // Terrains chauds et humides (haute altitude)
 				}
 			}
 		}
--- src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrainEnonce.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrainEnonce.java
@@ -6,13 +6,23 @@ import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
 import fr.ecole3il.rodez2023.perlin.terrain.visualisation.*;
 
 public class VisualiseurTerrainEnonce extends VisualiseurTerrain {
-	//public VisualiseurTerrainEnonce(Carte c) {
-		//super(new DetermineurTerrainEnonce(), c);
-	//}
 	private DetermineurTerrainEnonce determineur = new DetermineurTerrainEnonce();
+
+	/**
+	 * Constructeur qui initialise le visualiseur avec une carte donnée
+	 * @param carte La carte sur laquelle le visualiseur opère
+	 */
 	public VisualiseurTerrainEnonce(Carte carte) {
+		// Appel du constructeur de la classe parente
 		super(carte);
 	}
+
+	/**
+	 * Détermine le type de terrain à une position spécifique sur la carte
+	 * @param x La coordonnée horizonatale de la position sur la carte
+	 * @param y La coordonnée verticale de la position sur la carte
+	 * @return Le TypeTerrain correspondant aux caractéristiques du terrain à la position (xy)
+	 */
 	public TypeTerrain getTypeTerrain(int x, int y) {
 		Terrain terrain = getCarte().getTerrain(x,y);
 		return determineur.determinerTerrain(
